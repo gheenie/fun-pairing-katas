cef71500364670afa1323eef624636faf086eab4
infoomics <sbx_94@hotmail.com>
2023-01-20 02:51:46 +0000

    better test descriptions; created more robust tests

--- __tests__/check-usernames.test.js
+++ __tests__/check-usernames.test.js
@@ -1,38 +1,31 @@
 const checkUsernames = require("../katas/check-usernames");
 
-/* TODO
-
-["abc"] -> false
-["abcde", "abcdef"] -> true
-["abcde", "abcdefghijklmnopqrstu"] -> false
-["User"] -> false
-["abc2de", "abc_def"] -> true
-["abc2de", "abC def"] -> true
-
-*/
-
 describe("checkUsernames takes an array of usernames and returns true if they are all valid and false otherwise", () => {
-    test("at least 5 characters long", () => {
-        expect( checkUsernames(["abc", "abcdef"])).toBe(false);
+    test("some items < 5 chars; short by 1", () => {
+        expect( checkUsernames(["abcd", "abcdef"]) ).toBe(false);
     });
 
-    test("at least 5 characters long", () => {
-        expect( checkUsernames(["abcde", "abcdef"])).toBe(true);
+    test("all items >= 5 characters; some exactly 5", () => {
+        expect( checkUsernames(["abcde", "abcdef"]) ).toBe(true);
     });
 
-    test("no longer than 20 characters", () => {
-        expect( checkUsernames(["abcde", "abcdefghijklmnopqrstu"])).toBe(false);
+    test("some items > 20 characters; longer by 1", () => {
+        expect( checkUsernames(["abcde", "abcdefghijklmnopqrstu"]) ).toBe(false);
     });
 
-    test("only contains lowercase letters, numbers and underscores", () => {
-        expect( checkUsernames(["User"])).toBe(false);
+    test("all items <= 20 characters, some exactly 20", () => {
+        expect( checkUsernames(["abcde", "abcdefghijklmnopqrst"]) ).toBe(true);
     });
 
-    test('["abc2de", "abc_def"] -> true', () => {
-        expect( checkUsernames(["abc2de", "abc_def"])).toBe(true);
+    test('numbers and underscores; length valid', () => {
+        expect( checkUsernames(["12_45", "a_345678901234567890"]) ).toBe(true);
     });
 
-    test('["abc2de", "abC def"] -> false', () => {
-        expect( checkUsernames(["abC def", "abc_def"])).toBe(false);
+    test("uppercase letters; length valid", () => {
+        expect( checkUsernames(["Abcde", "12345678901234567890"]) ).toBe(false);
+    });
+
+    test('various invalid characters', () => {
+        expect( checkUsernames(["abC def", "abc_def"]) ).toBe(false);
     });
 })
