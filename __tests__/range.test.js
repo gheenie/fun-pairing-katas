ff6ecf621d7f90c8e62e34220c3f684d31ce67d1
infoomics <sbx_94@hotmail.com>
2023-01-20 02:53:50 +0000

    better test descriptions

--- __tests__/range.test.js
+++ __tests__/range.test.js
@@ -1,19 +1,19 @@
 const range = require("../katas/range");
 
 describe("range", () => {
-    test("length=0", () => {
+    test("0 length", () => {
         expect( range(0) ).toEqual([])
     });
 
-    test("length=5", () => {
+    test("non-0 length", () => {
         expect( range(5) ).toEqual([0, 1, 2, 3, 4])
     });
 
-    test("length=5, start=2", () => {
+    test("start also specified", () => {
         expect( range(5, 2) ).toEqual([2, 3, 4, 5, 6])
     });
 
-    test("length=5, start=2, step=2", () => {
+    test("step also specified", () => {
         expect( range(5, 2, 2) ).toEqual([2, 4, 6, 8, 10])
     });
 })
