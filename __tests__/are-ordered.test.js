3d850a79a1af0a1b2bdce1af375ca36b18c68ce4
infoomics <sbx_94@hotmail.com>
2023-01-20 02:35:41 +0000

    changed test descriptions; added more robust tests

--- __tests__/are-ordered.test.js
+++ __tests__/are-ordered.test.js
@@ -1,33 +1,39 @@
 const areOrdered = require("../katas/are-ordered");
 
-/* TODO
-
-[] -> false
-[1] -> true
-[1, 2] -> true
-[1, 2, 2] -> false
-[1, 3, 5, 2] -> false
-[1, 3, 5, 7, 8] -> true
-*/
-
 describe("The function takes an array of numbers as an input and returns true if all the numbers are in ascending order and false if they are not", () => {
-    test("[] -> false", () => {
+    test("empty array", () => {
         expect( areOrdered([]) ).toBe(false);
     });
 
-    test("[1] -> true", () => {
+    test("one item", () => {
         expect( areOrdered([1]) ).toBe(true);
     });
 
-    test("[1, 2] -> true", () => {
+    test("multiple items; ordered", () => {
         expect( areOrdered([1, 2]) ).toBe(true);
-    });
-
-    test("[1, 2, 2] -> false", () => {
-        expect( areOrdered([1, 2, 2]) ).toBe(false);
-    });
-
-    test("[1, 3, 5, 7, 8] -> true", () => {
         expect( areOrdered([1, 3, 5, 7, 8]) ).toBe(true);
+    });
+
+    test("multiple items; unordered", () => {
+        expect( areOrdered([2, 1]) ).toBe(false);
+        expect( areOrdered([8, 1, 3]) ).toBe(false);
+    });
+
+    test("multiple items; ordered; some same numbers", () => {
+        expect( areOrdered([1, 2, 2]) ).toBe(true);
+    });
+    
+    test("multiple items; unordered; some same numbers", () => {
+        expect( areOrdered([2, 1, 2]) ).toBe(false);
+    });
+
+    test("multiple items; ordered; different number of digits", () => {
+        expect( areOrdered([1, 3, 5, 17, 28]) ).toBe(true);
+        expect( areOrdered([1, 3, 5, 14, 23]) ).toBe(true);
+    });
+
+    test("multiple items; unordered; different number of digits", () => {
+        expect( areOrdered([1, 17, 3, 5, 28]) ).toBe(false);
+        expect( areOrdered([1, 23, 5, 3, 14]) ).toBe(false);
     });
 })
